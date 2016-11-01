4326cb481547bc284fd643befa0f0a85fd17fbbf
Fozzzz <fozzzz@dingtalk.com>
2016-11-01 12:27:36 +0800

    实体添加

--- src/main/src/ssh/web/TestWeb.java
+++ src/main/src/ssh/web/TestWeb.java
@@ -3,6 +3,7 @@ package ssh.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ssh.dao.TestDao;
 
 /**
@@ -13,7 +14,7 @@ public class TestWeb {
     @Autowired
     private TestDao testDao;
     @RequestMapping("test")
-    public String test(){
+    public @ResponseBody String test(){
         testDao.add();
         return "index";
     }
