75260c84e15a85315ec398e9e5c6b8ede374c86b
rojian <Rojian5495.com>
2016-11-01 14:08:41 +0800

    mainController

--- src/main/src/ssh/web/GoodsWeb.java
+++ src/main/src/ssh/web/GoodsWeb.java
@@ -4,25 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import ssh.dao.NewsDao;
-import ssh.entity.News;
+import ssh.dao.GoodDao;
+import ssh.entity.Goods;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 显示商品的信息
  * Created by Administrator on 2016/11/1.
  */
-@Controller("/main")
-public class NewsWeb {
+@Controller("/goods")
+public class GoodsWeb {
     @Autowired
-    private NewsDao newsDao;
-
-    @RequestMapping("showNewsList")
+    private GoodDao goodDao;
+    @RequestMapping("/showGoods")
     public @ResponseBody
-    List<News> showNews(String type){
-        List<News> newsList =new ArrayList<News>();
-        newsList =newsDao.get(0,4,type);
-        return newsList;
+    List<Goods> showGoods(){
+        List<Goods> goodsList=new ArrayList<Goods>();
+        goodsList=goodDao.get(0,20);
+        return goodsList;
     }
 }
