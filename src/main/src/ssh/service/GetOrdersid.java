9f125d46892775ef43ed29b8a256433195c5fa07
rojian <Rojian5495.com>
2016-11-01 18:50:40 +0800

    mainController

---
+++ src/main/src/ssh/service/GetOrdersid.java
@@ -0,0 +1,22 @@
+package ssh.service;
+
+import java.util.Random;
+
+/**
+ * Created by Administrator on 2016/11/1.
+ */
+public class GetOrdersid {
+    public static String getOrdersid(){
+        String id;
+        Random r = new Random();
+
+        id = System.nanoTime() + "" + r.nextInt();
+
+
+        return id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getOrdersid());
+    }
+}
--- src/main/src/ssh/web/OrdersWeb.java
+++ src/main/src/ssh/web/OrdersWeb.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 /**
  * Created by Administrator on 2016/11/1.
  */
-@Controller("/order")
+@Controller
+
 public class OrdersWeb {
 }
